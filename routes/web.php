f04e1418b7ef3b2e7181f0b50899f73713ca4f0e
Nuevo proyecto <8432300084@univermilenium.edu.mx>
2025-05-17 14:29:49 -0600

    Sergio clase1

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,18 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+//------------ declaracion de ruta fijas -------------
+Route::get('/Productos',function(){
+    return "Seccion de productos";
+});
+
+//----------- declaracion con parametros --------------
+Route::get('Productos/{id}', function ($id){
+    return "Bienvenido $id";
+});
+
+Route::get('prueba_parametros/{id}/{id2}', function($id,$id2){
+    $d= $id;
+    $nombre = explode("-",$d);
+    return "tu nombre es $nombre[1] $id2";
+});
